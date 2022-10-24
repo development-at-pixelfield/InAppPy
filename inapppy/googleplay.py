a388b7248b33fc42b3535e16e90cf2d7d2773a96
Michal Vávra <vavra@pixelfield.cz>
2022-10-24 11:20:12 +0200

    Update googleplay.py
    Change "cancelReason" -> 0 to be valid canceled value

--- inapppy/googleplay.py
+++ inapppy/googleplay.py
@@ -210,8 +210,8 @@ class GooglePlayVerifier:
             result = self.check_purchase_subscription(purchase_token, product_sku, service)
             verification_result.raw_response = result
 
-            cancel_reason = int(result.get("cancelReason", 0))
-            if cancel_reason != 0:
+            cancel_reason = int(result.get("cancelReason", None))
+            if cancel_reason is not None:
                 verification_result.is_canceled = True
 
             ms_timestamp = result.get("expiryTimeMillis", 0)
